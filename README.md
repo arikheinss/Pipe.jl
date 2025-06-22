1b722e5c10462711ef1786a6ef68a3a7f132f0df
arik heinss <arikheinss@yahoo.de>
2025-06-22 02:00:33 +0200

    added Stuff

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Pipe.jl
+
+Exports a single macro `@>` wich threads the result of one function call into the placeholder within the next expresseion. Default Placeholder is `_`. Flattens any `begin ... end` block.
+
+Example:
+```julia
+julia> @> [1, 2, 3] begin
+    map( sin, _)
+    reverse
+end
+```
+
+    
---
+++ src/pipe.jl
@@ -0,0 +1,48 @@
+using Base.Iterators:flatmap
+
+_inject(search :: Symbol, replace_with :: Any, into :: Any) = into
+_inject(search :: Symbol, replace_with :: Any, into :: Union{Tuple, Array}) = replace(into, search=>replace_with)
+_inject(search :: Symbol, replace_with :: Any, into :: Expr) = 
+    Expr( into.head, _inject(search, replace_with, into.args)...)
+
+# pipe: the function that transforms a sequence of expressions into the correct form for the @> macro
+pipe(s :: Symbol, e) =  e
+pipe(s :: Symbol, expr1, expr2, exprs...) = pipe(s, pipe(s, expr1, expr2), exprs...)
+pipe(s :: Symbol, expr, expr2 :: Any) = Expr(:call, expr2, expr)
+
+pipe(s :: Symbol, expr, expr2 :: Expr) = begin 
+    varname = gensym(s)
+    body = _inject(s, varname, expr2)
+    quote
+        let $varname = $expr 
+            $body
+        end
+    end
+end
+
+
+
+
+const default_symbol = Symbol("_")
+macro >(e1, stuff...)
+    # first, flatten out any begin ... end blocks
+    arg_to_iter(arg) = 
+        if arg isa Expr && arg.head == :block
+            filter(l -> !(l isa LineNumberNode), arg.args)
+        else
+            (arg,)
+        end
+    args = flatmap(arg_to_iter, stuff) 
+
+    if e1 isa Symbol
+        pipe(e1, args...)
+    else
+        pipe(default_symbol, arg_to_iter(e1)..., args...)
+    end
+end
+
+macro 🍛(exprs...)
+    return :(println("🍛"^$(length(exprs))))
+end
+
+@🍛 println("asdf") hello 12 begin end
